fa11d6d4ca915740c062607b9291ba93e25296ea
siddhesh <siddheshbadnapurkar@gmail.com>
2025-09-01 05:16:29 +0000

    increased abstraction further

--- go_back_arq/src/main.cpp
+++ go_back_arq/src/main.cpp
@@ -8,30 +8,36 @@
 #include "packet.hpp"
 #include "utils.hpp"
 
-// Function to generate a packet
-std::pair<std::vector<int>, std::string> generatePacket() {
-    // Init a std::vector of <int> of values::WINDOW_SIZE
+// function to generate a packet
+Packet generatePacket() {
+    // init a std::vector of values::WINDOW_SIZE
     std::vector<int> data(values::WINDOW_SIZE);
-    // fill the vector with random values
+    // fill the vector with random values from random_.hpp
     for (size_t i = 0; i < values::WINDOW_SIZE; i++) {
         data[i] = random_::getInt(values::MINIMUM_VALUE, values::MAXIMUM_VALUE);
     }
-    // get the current time in [date-month-year HH:MM:SS] format
+    // get the timestamp for the packet from utils.hpp
     std::string timestamp = getTime();
-    std::pair<std::vector<int>, std::string> packet = std::make_pair(data, timestamp);
-    return packet;
+    // return an object of class Packet
+    return Packet(data, timestamp);
 }
 
-void printPacket(std::pair<std::vector<int>, std::string>& packet) {
+// Function to print the contents of the packet
+void printPacket(Packet packet_) {
+    // get the data[] from packet_
+    std::vector<int> data = packet_.get_packet_data();
+    // get the timestamp from packet_
+    std::string timestamp = packet_.get_packet_timestamp();
+    // Formatted printing for the object data
     std::cout << '[';
     for (size_t i = 0; i < values::WINDOW_SIZE; i++) {
-        std::cout << packet.first[i] << (i == values::WINDOW_SIZE - 1 ? "]" : ", ");
+        std::cout << data[i] << (i == values::WINDOW_SIZE - 1 ? "]" : ", ");
     }
-    std::cout << " | [" << packet.second << ']' << std::endl;
+    std::cout << " | [" << timestamp << ']' << std::endl;
 }
 
 void test() {
-    auto i = generatePacket();
+    Packet i = generatePacket();
     printPacket(i);
 }
 
--- go_back_arq/src/packet.hpp
+++ go_back_arq/src/packet.hpp
@@ -4,6 +4,21 @@
 #include <string>
 
 class Packet {
+private:
     std::vector<int> data;
-    std::string time;
+    std::string timestamp;
+
+public:
+    Packet(std::vector<int> data_, std::string timestamp_) {
+        this -> data = data_;
+        this -> timestamp = timestamp_;
+    }
+
+    std::vector<int> get_packet_data() {
+        return this -> data;
+    }
+
+    std::string get_packet_timestamp() {
+        return this -> timestamp;
+    }
 };
